08a458da9f6edd8df5122570fea40f82585e7cb6
sujalp7420 <sujalpatel2726@gmail.com>
2026-02-08 17:18:19 +0530

    auth for faculty

--- generator/admin.py
+++ generator/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, Department, Subject, ClassRoom, TimeTable
+
+admin.site.register(User)
+admin.site.register(Department)
+admin.site.register(Subject)
+admin.site.register(ClassRoom)
+admin.site.register(TimeTable)
